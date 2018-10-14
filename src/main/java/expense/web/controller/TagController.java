99bcd71ef0f1783b137b177126666ed47d2e182d
ovidiu <enescu_vd@yahoo.com>
2018-10-14 11:41:54 +0300

    added controller method for add tag view

--- src/main/java/expense/web/controller/TagController.java
+++ src/main/java/expense/web/controller/TagController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -21,7 +22,13 @@ public class TagController {
     public String getTags(Model model) {
         List<Tag> tags = tagService.findAll();
         model.addAttribute("tags", tags);
-        return "/tags-listing";
+        return "tags-listing";
+    }
+
+    @RequestMapping(value = {"/tags/add"})
+    public String addTag(Tag tag) {
+        tagService.save(tag);
+        return "tag-add";
     }
 
 }
