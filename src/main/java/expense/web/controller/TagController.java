e155ad7e6ee9deb5cef4011bc7e79d95bb86b1d2
ovidiu <enescu_vd@yahoo.com>
2018-10-14 19:46:02 +0300

    implemented add tag screen and working save

--- src/main/java/expense/web/controller/TagController.java
+++ src/main/java/expense/web/controller/TagController.java
@@ -2,11 +2,14 @@ package expense.web.controller;
 
 import expense.model.Tag;
 import expense.service.TagService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -18,17 +21,34 @@ public class TagController {
     @Autowired
     private TagService tagService;
 
-    @GetMapping("/tags")
+    private Logger logger = LoggerFactory.getLogger(TagController.class);
+
+    public TagController() {
+    }
+
+    @RequestMapping({"/tags"})
     public String getTags(Model model) {
+        logger.info("Fetching tags route");
         List<Tag> tags = tagService.findAll();
         model.addAttribute("tags", tags);
         return "tags-listing";
     }
 
-    @RequestMapping(value = {"/tags/add"})
-    public String addTag(Tag tag) {
-        tagService.save(tag);
+    @RequestMapping({"tags/add"})
+    public String addTag(Model model) {
+        logger.info("Fetch add view");
+        model.addAttribute("tag", new Tag());
         return "tag-add";
+    }
+
+    @RequestMapping(
+            value = {"/tags"},
+            method = {RequestMethod.POST}
+    )
+    public String addNewTag(Tag tag, RedirectAttributes redirectAttributes) {
+        logger.info("Saving called for tag: {}", tag);
+        this.tagService.save(tag);
+        return "redirect:/tags";
     }
 
 }
