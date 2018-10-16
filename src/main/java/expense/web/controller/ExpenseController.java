58b0ff7663de101103ac6545083d6dafe9ac5934
ovidiu <enescu_vd@yahoo.com>
2018-10-16 22:25:36 +0300

    added expense add screen

--- src/main/java/expense/web/controller/ExpenseController.java
+++ src/main/java/expense/web/controller/ExpenseController.java
@@ -1,11 +1,16 @@
 package expense.web.controller;
 
 import expense.model.Expense;
+import expense.service.CategoryService;
 import expense.service.ExpenseService;
+import expense.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -16,12 +21,30 @@ import java.util.List;
 public class ExpenseController {
     @Autowired
     private ExpenseService expenseService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private TagService tagService;
 
     @GetMapping("/expenses")
     public String getExpenses(Model model) {
         List<Expense> expenses = expenseService.findAll();
         model.addAttribute("expenses", expenses);
         return "expenses-listing";
+    }
+
+    @RequestMapping({"expenses/add"})
+    public String addExpense(Model model) {
+        model.addAttribute("tags", tagService.findAll());
+        model.addAttribute("categories", categoryService.findAll());
+        model.addAttribute("expense", new Expense());
+        return "expense-add";
+    }
+
+    @RequestMapping(value = {"/expenses"}, method = {RequestMethod.POST})
+    public String addNewExpense(Expense expense, RedirectAttributes redirectAttributes) {
+        expenseService.save(expense);
+        return "redirect:/expenses";
     }
 
 }
---
+++ src/main/resources/static/js/add-expense.js
@@ -0,0 +1,34 @@
+console.log(TAGS);
+console.log(CATEGORIES);
+
+initializeSelect("selectTag", TAGS, true, formatEntryDisplay);
+initializeSelect("selectCat", CATEGORIES, false, formatEntryDisplay);
+
+function formatEntryDisplay(entry) {
+    if (!entry.id) {
+        return entry.text;
+    }
+
+    let $entry = $(
+        '<div class="ds-table full-width">' + entry.text + '<span class="select-color" style="background: ' + entry.color + ';"></span></div>'
+    );
+    return $entry;
+}
+
+function initializeSelect(id, arr, multiple = false, templateResult) {
+    let arrForSelect = arr.map(e => {
+            return {
+                id: e.id,
+                text: e.name,
+                color: e.color
+            };
+        }
+    );
+    id = "#" + id;
+    $(id).select2({
+        data: arrForSelect,
+        multiple: multiple,
+        allowClear: multiple,
+        templateResult: templateResult
+    });
+}
