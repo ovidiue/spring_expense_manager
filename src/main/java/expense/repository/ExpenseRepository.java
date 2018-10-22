946113e5622ef2bd8b55e3ab0b8aa5c7ee9b0dd4
ovidiu <enescu_vd@yahoo.com>
2018-10-22 23:38:34 +0300

    refactored to jparepository

--- src/main/java/expense/repository/ExpenseRepository.java
+++ src/main/java/expense/repository/ExpenseRepository.java
@@ -1,7 +1,7 @@
 package expense.repository;
 
 import expense.model.Expense;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +10,6 @@ import java.util.List;
  * Created by Ovidiu on 10-Oct-18.
  */
 @Repository
-public interface ExpenseRepository extends CrudRepository<Expense, Long> {
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     List<Expense> findAll();
 }
