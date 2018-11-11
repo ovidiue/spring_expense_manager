4ee0795268cc49a277d4988cb5fefb8569820ea9
ovidiu <enescu_vd@yahoo.com>
2018-11-11 02:28:35 +0200

    added rateService findAll method

--- src/main/java/expense/repository/RateRepository.java
+++ src/main/java/expense/repository/RateRepository.java
@@ -1,12 +1,12 @@
 package expense.repository;
 
 import expense.model.Rate;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by Ovidiu on 10-Oct-18.
  */
 @Repository
-public interface RateRepository extends CrudRepository<Rate, Long> {
+public interface RateRepository extends JpaRepository<Rate, Long> {
 }
--- src/main/java/expense/service/RateService.java
+++ src/main/java/expense/service/RateService.java
@@ -1,8 +1,11 @@
 package expense.service;
 
+import expense.model.Rate;
 import expense.repository.RateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Ovidiu on 10-Oct-18.
@@ -11,4 +14,8 @@ import org.springframework.stereotype.Service;
 public class RateService {
     @Autowired
     private RateRepository rateRepository;
+
+    public List<Rate> findAll() {
+        return rateRepository.findAll();
+    }
 }
