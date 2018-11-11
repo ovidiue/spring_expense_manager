c3000dea8a617c3697d812314ca92925fd5c9fd0
ovidiu <enescu_vd@yahoo.com>
2018-11-11 11:36:20 +0200

    added RateController edit screen route

--- src/main/java/expense/web/controller/RateController.java
+++ src/main/java/expense/web/controller/RateController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -40,6 +41,14 @@ public class RateController {
         model.addAttribute("rate", new Rate());
         return "rate-add";
     }
+
+    @GetMapping("/rates/edit/{rateId}")
+    public String getEditRateView(@PathVariable Long rateId, Model model) {
+        Rate rate = this.rateService.findById(rateId).get();
+        model.addAttribute("rate", rate);
+        return "rate-add";
+    }
+
 
     @RequestMapping(value = {"/rates/save"}, method = {RequestMethod.POST})
     public String saveRate(Rate rate, RedirectAttributes redirectAttributes) {
