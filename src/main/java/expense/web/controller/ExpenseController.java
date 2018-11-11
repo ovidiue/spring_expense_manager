a879892014b8dbc735e3e6ff17db7c162116dcfa
ovidiu <enescu_vd@yahoo.com>
2018-11-11 02:27:03 +0200

    fixed lazy loading for rates

--- src/main/java/expense/web/controller/ExpenseController.java
+++ src/main/java/expense/web/controller/ExpenseController.java
@@ -7,6 +7,7 @@ import expense.service.TagService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,6 +20,7 @@ import java.util.List;
  */
 @Controller
 @Slf4j
+@Transactional
 public class ExpenseController {
     @Autowired
     private ExpenseService expenseService;
