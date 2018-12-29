9253757026f0659671871816c0c491b17ab3015f
ovidiu <enescu_vd@yahoo.com>
2018-12-29 13:48:31 +0200

    added dashboard controller, html and js

--- src/main/java/expense/web/controller/DashboardController.java
+++ src/main/java/expense/web/controller/DashboardController.java
@@ -1,7 +1,13 @@
 package expense.web.controller;
 
+import expense.repository.CategoryStats;
+import expense.service.CategoryService;
+import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,9 +18,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/dashboard")
 public class DashboardController {
 
+  @Autowired
+  private CategoryService categoryService;
+
+
   @RequestMapping("")
-  public String getDasboardRoute() {
+  public String getDasboardRoute(Model model) {
+    Map<String, Integer> categoryCount = this.categoryService.getCategoryCount();
+    model.addAttribute("categoryCount", categoryCount);
+    List<CategoryStats> categoryStats = this.categoryService.getCategoryInfo();
+    model.addAttribute("categoryStats", categoryStats);
+    log.info("categoryStats {}", categoryStats);
     return "dashboard";
   }
+
 
 }
--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,1 +1,76 @@
 console.log('dashboard loaded');
+initializePieCategory();
+initializeBarchartCategory();
+
+// *********************************************************
+// *********************************************************
+// *********************************************************
+
+function initializePieCategory() {
+  am4core.useTheme(am4themes_animated);
+  var chart = am4core.create("pieHolder", am4charts.PieChart);
+
+// Add data
+  chart.data = categoryStats;
+  chart.legend = new am4charts.Legend();
+
+// Add and configure Series
+  var pieSeries = chart.series.push(new am4charts.PieSeries());
+  pieSeries.dataFields.value = "total";
+  pieSeries.dataFields.category = "name";
+  pieSeries.slices.template.stroke = am4core.color("#fff");
+  pieSeries.slices.template.strokeWidth = 2;
+  pieSeries.slices.template.strokeOpacity = 1;
+
+// This creates initial animation
+  pieSeries.hiddenState.properties.opacity = 1;
+  pieSeries.hiddenState.properties.endAngle = -90;
+  pieSeries.hiddenState.properties.startAngle = -90;
+}
+
+function initializeBarchartCategory() {
+  am4core.useTheme(am4themes_animated);
+
+  var chart = am4core.create("barHolder", am4charts.XYChart);
+  chart.data = categoryStats;
+
+  chart.legend = new am4charts.Legend();
+
+  chart.padding(40, 40, 40, 40);
+
+  var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+  categoryAxis.renderer.grid.template.location = 0;
+  categoryAxis.dataFields.category = "name";
+  categoryAxis.renderer.minGridDistance = 60;
+
+  var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+  valueAxis.min = 0;
+  valueAxis.max = 24000;
+  valueAxis.strictMinMax = true;
+  valueAxis.renderer.minGridDistance = 30;
+
+  valueAxis.renderer.labels.template.hiddenState.transitionDuration = 2000;
+  valueAxis.renderer.labels.template.defaultState.transitionDuration = 2000;
+
+// axis break
+  var axisBreak = valueAxis.axisBreaks.create();
+  axisBreak.startValue = 2100;
+  axisBreak.endValue = 22900;
+  axisBreak.breakSize = 0.01;
+
+// make break expand on hover
+  var hoverState = axisBreak.states.create("hover");
+  hoverState.properties.breakSize = 1;
+  hoverState.properties.opacity = 0.1;
+  hoverState.transitionDuration = 1500;
+
+  axisBreak.defaultState.transitionDuration = 1000;
+
+  var series = chart.series.push(new am4charts.ColumnSeries());
+  series.dataFields.categoryX = "name";
+  series.dataFields.valueY = "total";
+  series.columns.template.tooltipText = "{valueY.payed}";
+  series.columns.template.tooltipY = 0;
+  series.columns.template.strokeOpacity = 0;
+
+}
